a2643e57174955921e38d19a8489cc5f1245c79c
vates_abiss <vates_abiss@lnexternos.com.ar>
2020-12-20 01:40:57 -0300

    docs fix

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,7 +6,7 @@ import { ContactComponent } from './pages/contact/contact.component';
 const routes: Routes = [
   { path: 'home', component: HomeComponent },
   { path: 'contact', component: ContactComponent },
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  // { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: '**', component: ContactComponent }
 ];
 
